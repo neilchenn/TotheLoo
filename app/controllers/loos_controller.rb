3ece6a9e37c1f0f3a475a2119ba75c884319c472
Kevin Nguyen <kevintngu@hotmail.com>
2022-06-07 11:21:43 +1000

    adding routes

--- app/controllers/loos_controller.rb
+++ app/controllers/loos_controller.rb
@@ -1,2 +1,42 @@
 class LoosController < ApplicationController
+  def index
+    @loos = Loo.all
+  end
+
+  def show
+    @loo = Loo.find(params[:id])
+  end
+
+  def new
+    @loo = Loo.new
+  end
+
+  def create
+    @loo = Loo.new(loo_params)
+    if @loo.save
+      redirect_to loos_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @loos = Loo.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+    @booking.destroy
+  end
+
+  def navigation
+  end
+
+  def favourite
+  end
+
+  def unfavourite
+  end
 end

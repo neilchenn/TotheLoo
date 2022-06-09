6cf1a39699cf728305edc90db5c55c69406362de
Shannon Gordon <shazkroeger@gmail.com>
2022-06-09 17:47:43 +1000

    styling index

--- db/migrate/20220607090905_change_review_columns_to_decimal.rb
+++
@@ -1,9 +0,0 @@
-class ChangeReviewColumnsToDecimal < ActiveRecord::Migration[6.1]
-  def change
-    change_column :reviews, :cleanliness, :decimal,  precision: 3, scale: 2
-    change_column :reviews, :flushing_power, :decimal,  precision: 3, scale: 2
-    change_column :reviews, :ambience, :decimal,  precision: 3, scale: 2
-    change_column :reviews, :toilet_paper_soap, :decimal,  precision: 3, scale: 2
-    change_column :reviews, :star_rating, :decimal,  precision: 3, scale: 2
-  end
-end

5e8e01fbb727d5e70c5e513633646154ce1bdad8
Shannon Gordon <shazkroeger@gmail.com>
2022-06-10 15:03:25 +1000

    review styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_07_090905) do
-
+ActiveRecord::Schema.define(version: 2022_06_10_014403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

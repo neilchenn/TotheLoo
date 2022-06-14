1096af23c654954cef328cd2348b47b920283999
neil chen <zooming.in@hotmail.com>
2022-06-14 11:33:10 +1000

    nav

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -32,7 +32,7 @@ export default class extends Controller {
         // Draw an arrow next to the location dot to indicate which direction the device is heading.
         showUserHeading: true,
         }),
-        "top-right"
+        "bottom-right"
       );
 
       // To get the current position

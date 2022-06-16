a0e5b9c6df4fa87f5a14647836b1c3e9a94ff720
Kevin Nguyen <kevintngu@hotmail.com>
2022-06-16 10:34:31 +1000

    meta stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :show_navbar!
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   private
 
   def show_navbar!
